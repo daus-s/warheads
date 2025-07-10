0cb9ba627f689f793507a5f28527e550e5955374
Daus <dauscarmichael@gmail.com>
2025-07-10 11:40:24 -0700

    corrected main function calls

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ use warheads::dapi::hunting::{chronicle_nba, observe_nba, revise_nba};
 async fn main() {
     println!("hello, {}!", "lisan al-gaib"); //TODO: make this say hi to the user with auth/name
 
-    //observe_nba().await;
+    observe_nba().await;
 
-    // revise_nba();
+    revise_nba();
 
     chronicle_nba().await;
 }
